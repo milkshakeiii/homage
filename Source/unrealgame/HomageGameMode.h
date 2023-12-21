ebe32ec764949f99c731871a7723fbb70babc60f
Henry Solberg <milkshakeiii@gmail.com>
2023-12-20 18:52:32 -0800

    constrain to plane

---
+++ Source/unrealgame/HomageGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "HomageGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGAME_API AHomageGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
